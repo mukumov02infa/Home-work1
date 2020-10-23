5e7056bab8bb3d19b2e55cc787d93d9abe4ffe09
mukumov02infa <73363179+mukumov02infa@users.noreply.github.com>
2020-10-24 02:03:45 +0300

    Create 1_2Б.cpp

---
+++ 1_2Б.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int n = 40, m, j, i, f;
+	double h, Res;
+
+	double* z = new double[n];
+
+
+	cout << "m = ";
+	cin >> m;
+	cout << "h = ";
+	cin >> h;
+
+	cout << "z0 = ";
+	cin >> z[0];
+	cout << "z1 = ";
+	cin >> z[1];
+
+	for (i = 2; i <= n; i++)
+	{
+		z[i] = (1 - z[i - 1]) / (1 + z[i - 2] * z[i - 2]) * sin(z[i - 1] * z[i - 1] + z[i - 1] * z[i - 2] + 2);
+    z[i] = abs(z[i]);
+		cout << "z[" << i << "] = " << z[i] << endl;
+	}
+
+	for (i = 1; i <= m; i++)
+	{
+		f = 0;
+		for (j = 1; j < n; j++)
+		{
+			if (z[j] >= (i - 1) * h && z[j] < i * h)
+				f++;
+
+		}
+		cout << "f =" << f << endl;
+	}		
+}
